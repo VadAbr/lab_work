e9254a0604652763926763e72a0982bfbd14ca06
Vadim Abramov <vadimabramov322@gmail.com>
2022-11-25 13:54:09 +0300

    add mongoDB

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 import express from 'express';
+import mongoose from 'mongoose'
+const CountSchema = new mongoose.Schema({
+    count: Number
+})
+
+const UsersConnection = new mongoose.Schema({
+    dateTime: String,
+    userInfo: String
+})
+
+const CountModel = mongoose.model("Count", CountSchema)
+const UsersConnectionModel = mongoose.model("Users", UsersConnection)
+
+mongoose.connect('mongodb://mongodbuser:mongodbpassword@mongo:27017/', {useNewUrlParser: true})
+mongoose.connection.on('error', (e)=>console.log(e))
+mongoose.connection.once('open', ()=>console.log('CONNECTED'))
 
 const app = express();
 
-let count = 0;
+const userHandle = (r) => {
+    const newUser = new UsersConnectionModel({dateTime: (new Date()).toDateString(), userInfo: r.headers['user-agent']})
+    newUser.save((e, user)=>{
+        console.log("USER", user)
+    })
+}
 
 app
-.get('/', (r, res) => res.send(`${count}`))
-.get('/stat', (r, res) => {
-    const oldValue = count++;
-    res.send(`${oldValue}`)
+.get('/', async (r, res) => {
+    const count = await CountModel.findOne()
+    userHandle(r)
+    if(count) {
+        res.send(`${count.count}`)
+        return
+    }
+
+    const newCount = new CountModel({count: 0})
+    newCount.save((err, newCount)=>{
+        console.log("NEW COUNT", newCount)
+        res.send(`${newCount.count}`)
+    })
+})
+.get('/stat', async (r, res) => {
+    userHandle(r)
+    const count = await CountModel.findOne()
+    await CountModel.updateOne({_id: count._id}, {count: count.count + 1})
+    res.send(`${count.count + 1}`)
 })
 .get("/about", (r, res)=>{
+    userHandle(r)
     res.setHeader("Content-Type", "text/html");
     res.send(`
     <h3>Hello, it's Vadim Abramov!</h3>
-    <b>Hostname:</b> ${r.headers.host}<br/>
+    <b>Hostname:</b> ${r.headers['user-agent']}<br/>
     `)
 })
 
-app.listen(8080, ()=>console.log('started'));
+app.listen(5000, ()=>console.log('STARTED SERVER'));
