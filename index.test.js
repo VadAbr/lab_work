9f9f96480b299a3ebd547f95a2fe2af4725f6db2
Vadim Abramov <vadimabramov322@gmail.com>
2022-11-26 13:03:12 +0300

    fix test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,17 @@
 const request = require("supertest");
 const app = require("./index");
+const mongoose = require ('mongoose')
 
 describe("Test the test path", () => {
+  beforeAll(done => {
+    done()
+  })
+
+  afterAll(done => {
+    mongoose.connection.close()
+    done()
+  })
+
   test("It should response the GET method", () => {
     return request(app)
       .get("/test")
